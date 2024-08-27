f37d9921efeedf07ab1ee4928679a903801f1ea0
Pratham16112002 <prathamdhiman@rediffmail.com>
2024-08-27 06:58:25 +0530

    Open Project functionality

--- gui.go
+++ gui.go
@@ -7,9 +7,16 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+type gui struct {
+	win   fyne.Window
+	title binding.String
+}
 
 func makeBanner() fyne.CanvasObject {
 	toolbar := widget.NewToolbar(widget.NewToolbarAction(theme.HomeIcon(), func() {
@@ -21,14 +28,42 @@ func makeBanner() fyne.CanvasObject {
 	return container.NewStack(toolbar, container.NewPadded(logo))
 }
 
-func makeGUI() fyne.CanvasObject {
+func (g *gui) makeGUI() fyne.CanvasObject {
 	top := makeBanner()
 	left := widget.NewLabel("Left")
 	right := widget.NewLabel("Right")
-	content := canvas.NewRectangle(color.Gray{Y: 0xee})
+
+	directory := widget.NewLabelWithData(g.title)
+	content := container.NewStack(canvas.NewRectangle(color.Gray{Y: 0xee}), directory)
 	seperators := [3]fyne.CanvasObject{
 		widget.NewSeparator(), widget.NewSeparator(), widget.NewSeparator(),
 	}
-	obj := []fyne.CanvasObject{content, top, left, right, seperators[0], seperators[1], seperators[2]}
+	obj := []fyne.CanvasObject{
+		content,
+		top,
+		left,
+		right,
+		seperators[0],
+		seperators[1],
+		seperators[2],
+	}
 	return container.New(CustomLayout(top, left, right, content, seperators), obj...)
+}
+
+func (g *gui) openProjectDialog() {
+	dialog.ShowFolderOpen(func(dir fyne.ListableURI, e error) {
+		if e != nil {
+			dialog.ShowError(e, g.win)
+			return
+		}
+		if dir == nil {
+			return
+		}
+		g.openProject(dir)
+	}, g.win)
+}
+
+func (g *gui) openProject(dir fyne.ListableURI) {
+	g.title.Set(dir.Name())
+	return
 }
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/storage"
 )
 
 func main() {
@@ -10,6 +16,41 @@ func main() {
 	a.Settings().SetTheme(NewCustomTheme())
 	w := a.NewWindow("IDE")
 	w.Resize(fyne.NewSize(1024, 768))
-	w.SetContent(makeGUI())
+	win := &gui{win: w}
+	w.SetContent(win.makeGUI())
+	w.SetMainMenu(win.makeMenu())
+	win.title.AddListener(binding.NewDataListener(func() {
+		name, _ := win.title.Get()
+		w.SetTitle("IDE : " + name)
+	}))
+	flag.Usage = func() {
+		fmt.Println("Usage : IDE project [directory]")
+	}
+	flag.Parse()
+	if len(flag.Args()) > 0 {
+		dirPath := flag.Args()[0]
+		dirPath, err := filepath.Abs(dirPath)
+		if err != nil {
+			fmt.Println("Error resolving the path : ", dirPath)
+			return
+		}
+		dirURI := storage.NewFileURI(dirPath)
+		dir, err := storage.ListerForURI(dirURI)
+		if err != nil {
+			fmt.Println("Error opening project", err)
+			return
+		}
+		win.openProject(dir)
+	} else {
+		win.openProjectDialog()
+	}
 	w.ShowAndRun()
+}
+
+func (g *gui) makeMenu() *fyne.MainMenu {
+	file := fyne.NewMenu("File", fyne.NewMenuItem("Open project", func() {
+		g.openProjectDialog()
+	}))
+
+	return fyne.NewMainMenu(file)
 }
